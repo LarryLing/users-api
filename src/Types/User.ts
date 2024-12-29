e9a88e8c6cc8ef0ef4718a999fc0c46e951a2acd
CS376-Freestyle-Game <larryling.main@gmail.com>
2024-12-28 18:58:33 -0600

    Implemented GET method for getting individual users

---
+++ src/Types/User.ts
@@ -0,0 +1,14 @@
+export type User = {
+    id : number;
+    first_name : string;
+    last_name : string;
+    email : string;
+    bio : string;
+    date_of_birth : string;
+    pronouns : string;
+    major : string;
+    graduation_year : number;
+    created_at : string;
+    profile_picture : string;
+    background_picture : string;
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import { Pool } from 'pg';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { User } from "./Types/user";
 
 dotenv.config();
 
@@ -16,11 +17,50 @@ const pool = new Pool({
     },
 });
 
-app.get("/api", (req : Request, res : Response) => {
+app.get(
+    "/api", 
+    (req : Request, res : Response) => {
     res.json({ "message" : "Hello world!" });
-});
+    }
+);
+
+app.get(
+    "/api/users", 
+    async (req : Request, res : Response) => {
+        try {
+            const result = await pool.query<User>(
+                "SELECT * FROM users ORDER BY id ASC"
+            );
+            res.json(result.rows);
+        } catch (error) {
+            console.error("Query error: ", error);
+            res.status(500).json({ error : "Failed to fetch users" });
+        }
+    }
+);
+
+app.get(
+    "/api/users/:id", 
+    async (req : Request, res : Response) => {
+        try {
+            const result = await pool.query<User>(
+                "SELECT * FROM users WHERE id = $1",
+                [req.params.id]
+            );
+            
+            // if (result.rows.length === 0) {
+            //     return res.status(404).json({ error : "User not found" });
+            // }
+
+            res.json(result.rows[0]);
+        } catch (error) {
+            console.error("Query error: ", error);
+            res.status(500).json({ error : "Failed to fetch users" });
+        }
+    }
+);
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });

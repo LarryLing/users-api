2e12bded164950cb40280df74d3aa2ee3ad1c576
CS376-Freestyle-Game <larryling.main@gmail.com>
2024-12-30 01:46:16 -0600

    Added CURD commands for user_profiles table

--- src/Types/User.ts
+++ src/Types/User.ts
@@ -5,18 +5,18 @@ export type User = {
     email : string;
     phone_number : string;
     hometown : string;
-
 }
 
 export type UserProfile = {
     id : number;
     user_id : number;
-    profile_picture : string;
-    background_picture : string;
+    profile_url : string;
+    background_url : string;
     major : string;
-    graduation_year : number;
+    class_of : number;
     bio : string;
     date_of_birth : string;
     pronouns : string;
     created_at : string;
+    connections : number[];
 }
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import { Pool } from 'pg';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { User } from "./Types/User";
+import { User, UserProfile } from "./Types/User";
 import { error } from "console";
 
 dotenv.config();
@@ -55,7 +55,7 @@ app.post("/api/users", async (request : Request, response : Response) => {
 
     try {
         await pool.query<User>(
-            "INSERT INTO users (id, first_name, last_name, email, phone_number, hometown) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
+            "INSERT INTO users (id, first_name, last_name, email, phone_number, hometown) VALUES ($1, $2, $3, $4, $5, $6)",
             [id, first_name, last_name, email, phone_number, hometown]
         );
         response.status(201).send(`User added with ID: ${ id }`);
@@ -98,7 +98,85 @@ app.delete("/api/users/:id", async (request : Request, response : Response) => {
         console.error("Query error: ", error);
         response.status(500).json({ error : "An internal server error occured!" });
     }
-})
+});
+
+app.get("/api/user_profiles", async (request : Request, response : Response) => {
+    try {
+        const result = await pool.query<UserProfile>("SELECT * FROM user_profiles ORDER BY id ASC");
+        response.status(200).json(result.rows);
+    } 
+    catch (error) {
+        console.error("Query error: ", error);
+        response.status(500).json({ error : "An internal server error occured!" });
+    }
+});
+
+app.get("/api/user_profiles/:id", async (request : Request, response : Response) => {
+    const parsedId = parseInt(request.params.id);
+    if (isNaN(parsedId)) response.status(400).json({ error : "Requested with an invalid ID!"});
+
+    try {
+        const users = await pool.query<UserProfile>("SELECT * FROM user_profiles WHERE id = $1", [request.params.id]);
+        if (users.rows.length === 0) {
+            response.status(404).json({ error : "Profile not found!" });
+        }
+        response.status(200).json(users.rows[0]);
+    } 
+    catch (error) {
+        console.error("Query error: ", error);
+        response.status(500).json({ error : "An internal server error occured!" });
+    }
+});
+
+app.post("/api/user_profiles", async (request : Request, response : Response) => {
+    const { id, user_id, profile_url, background_url, major, class_of, bio, date_of_birth, pronouns, created_at, connections } : UserProfile = request.body;
+
+    try {
+        await pool.query<UserProfile>(
+            "INSERT INTO user_profiles (id, user_id, profile_url, background_url, major, class_of, bio, date_of_birth, pronouns, created_at, connections) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
+            [id, user_id, profile_url, background_url, major, class_of, bio, date_of_birth, pronouns, created_at, connections]
+        );
+        response.status(201).send(`Profile added with ID: ${ id }`);
+    }
+    catch (error) {
+        console.error("Query error: ", error);
+        response.status(500).json({ error : "An internal server error occured!" });
+    }
+});
+
+app.put("/api/user_profiles/:id", async (request : Request, response : Response) => {
+    const parsedId = parseInt(request.params.id);
+    if (isNaN(parsedId)) response.status(400).json({ error : "Requested with an invalid ID!"});
+
+    const { id, user_id, profile_url, background_url, major, class_of, bio, date_of_birth, pronouns, created_at, connections } : UserProfile = request.body;
+
+    try {
+        await pool.query<UserProfile>(
+            "UPDATE user_profiles SET user_id=$1, profile_url=$2, background_url=$3, major=$4, class_of=$5, bio=$6, date_of_birth=$7, pronouns=$8, created_at=$9, connections=$10 WHERE id=$11",
+            [user_id, profile_url, background_url, major, class_of, bio, date_of_birth, pronouns, created_at, connections, parsedId]
+        );
+        response.status(200).send(`Profile modified with ID: ${ id }`);
+    }
+    catch (error) {
+        console.error("Query error: ", error);
+        response.status(500).json({ error : "An internal server error occured!" });
+    }
+});
+
+app.delete("/api/user_profiles/:id", async (request : Request, response : Response) => {
+    const parsedId = parseInt(request.params.id);
+    if (isNaN(parsedId)) response.status(400).json({ error : "Requested with an invalid ID!"});
+
+    try {
+        await pool.query<User>("DELETE FROM user_profiles WHERE id=$1", [parsedId]);
+    
+        response.status(200).send(`User deleted with ID: ${ parsedId }`);
+    }
+    catch (error) {
+        console.error("Query error: ", error);
+        response.status(500).json({ error : "An internal server error occured!" });
+    }
+});
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
